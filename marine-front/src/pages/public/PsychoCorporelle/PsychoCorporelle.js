a2934c4043195d0a68a5ac3979bc9b584f514dc9
Julien Wangon <julien.wangon@gmail.com>
2024-02-25 12:21:31 +0100

    ADD: create app page skeleton

---
+++ marine-front/src/pages/public/PsychoCorporelle/PsychoCorporelle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PsychoCorporelle = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default PsychoCorporelle;
---
+++ marine-front/src/pages/public/TherapieSystemique/Therapiesystemique.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Therapiesystemique = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Therapiesystemique;
