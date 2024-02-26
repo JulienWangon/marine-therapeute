649926613b41f4247865fa383c0b5b6a59f138a0
Julien Wangon <julien.wangon@gmail.com>
2024-02-26 18:50:34 +0100

    UPDATE: add routing on App.js

--- marine-front/src/App.js
+++ marine-front/src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/public/Home/Home.js';
+import MonParcours from './pages/public/MonParcours/MonParcours.js';
+import PsychoCorporelle from './pages/public/PsychoCorporelle/PsychoCorporelle.js';
+import Therapiesystemique from './pages/public/TherapieSystemique/Therapiesystemique.js';
+
 
 function App() {
   return (
     <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home/>}/>
+                <Route path="/accueil" element={<Home/>}/>
+                <Route path="/mon-parcours" element={<MonParcours/>}/>
+                <Route path="/therapie-systemique" element={<Therapiesystemique/>}/>
+                <Route path="/psychocorporelle" element={<PsychoCorporelle/>}/>
+            </Routes>        
+        </BrowserRouter>
 
     </div>
   );
