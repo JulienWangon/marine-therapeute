0fc2e6c75cf34b24b7dbcd5bd3659bf327e9f475
Julien Wangon <julien.wangon@gmail.com>
2024-02-29 10:46:53 +0100

    UPDATE: add navbar style

--- marine-front/src/components/common/Navbar/Navbar.js
+++ marine-front/src/components/common/Navbar/Navbar.js
@@ -1,49 +1,62 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Importation de useNavigate
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
-    const navigate = useNavigate(); // Initialisation de useNavigate
+    const navigate = useNavigate();
+    const [isClicked, setIsClicked] = useState(false); // État pour suivre si le bouton a été cliqué
 
     const links = [
-        {text: "Accueil", url: "/accueil"},
-        {text: "Mon parcours", url: "/mon-parcours"},
-        {text: "Thérapie Systèmique", url: "/therapie-systemique"},
-        {text: "Psychocorporelle", url: "/psychocorporelle"},
+        { text: "Accueil", url: "/accueil" },
+        { text: "Mon parcours", url: "/mon-parcours" },
+        { text: "Thérapie Systèmique", url: "/therapie-systemique" },
+        { text: "Psychocorporelle", url: "/psychocorporelle" },
     ];
 
-    // Fonction pour gérer la navigation
     const handleNavigate = (url) => {
-        navigate(url); // Utilisation correcte de navigate avec un seul argument
+        navigate(url);
+    };
+
+    const toggleColor = () => {
+        setIsClicked(!isClicked); // Basculer l'état du clic
     };
 
     return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-          <button className="navbar-toggler ma-toggler-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <i className="fa-solid fa-bars"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-
-              <ul className="navbar-nav navMenu">
-              {links.map((link, index) => (
-                  <li className="navItem" key={index}>
-                      <a 
-                          href={link.url} 
-                          onClick={(e) => {
-                              e.preventDefault(); // Empêche le comportement par défaut du lien
-                              handleNavigate(link.url); // Correction: utilisez uniquement l'URL
-                          }} 
-                          className="navLink"
-                      >
-                          {link.text}
-                      </a>
-                  </li>
-              ))}
-              </ul>
-          </div>
-      </div>        
-  </nav>
-
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <button 
+                    className="navbar-toggler ma-toggler-white" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNavAltMarkup" 
+                    aria-controls="navbarNavAltMarkup" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                    onClick={toggleColor} // Gérer le clic sur le bouton
+                    style={{ color: isClicked ? '#5A1D3E' : '' }} // Appliquer la couleur conditionnellement
+                >
+                    <i className="fa-solid fa-bars" style={{ color: isClicked ? '#5A1D3E' : '' }}></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <ul className="navbar-nav navMenu">
+                        {links.map((link, index) => (
+                            <li className="navItem" key={index}>
+                                <a 
+                                    href={link.url} 
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        handleNavigate(link.url);
+                                    }} 
+                                    className="navLink"
+                                >
+                                    {link.text}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 };
 
