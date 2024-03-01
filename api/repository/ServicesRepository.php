06f273f991b40dbe9f5bbff1bba5aadfb3afb58f
Julien Wangon <julien.wangon@gmail.com>
2024-03-01 13:04:55 +0100

    ADD: create get all services method on Services Repository

---
+++ api/repository/ServicesRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/../models/Database.php';
+require_once __DIR__ . '/../models/Services.php';
+
+class ServicesRepository extends Database {
+
+  public function getServices() :array {
+    try {
+      $db = $this->getBdd();
+      $req = "SELECT * FROM services";
+
+      $stmt = $db->prepare($req);
+      $stmt->execute();
+      $servicesData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      $services = [];
+      foreach($servicesData as $serviceData) {
+          $service = new Services(
+            $serviceData['id'],
+            $serviceData['name'],
+            $serviceData['pathImg']
+          );
+          $services[] = $service;
+      }
+      return $services;
+
+
+    } catch (PDOException $e) {
+      $this->handleException($e, "extraction de la liste des services");
+    }
+  }
+
+
+
+
+}
+
