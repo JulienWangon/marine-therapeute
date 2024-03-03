33007bb372e37f43c1e08f21c33ec3c420c26025
Julien Wangon <julien.wangon@gmail.com>
2024-03-03 14:20:30 +0100

    UPDATE: add services context on app.js

--- marine-front/src/App.js
+++ marine-front/src/App.js
@@ -6,20 +6,22 @@ import MonParcours from './pages/public/MonParcours/MonParcours.js';
 import PsychoCorporelle from './pages/public/PsychoCorporelle/PsychoCorporelle.js';
 import Therapiesystemique from './pages/public/TherapieSystemique/Therapiesystemique.js';
 
+import { ServicesProvider } from './context/ServicesContext.js';
 
 function App() {
   return (
     <div className="App">
+      <ServicesProvider>
         <BrowserRouter>
-            <Routes>
-                <Route index element={<Home/>}/>
-                <Route path="/accueil" element={<Home/>}/>
-                <Route path="/mon-parcours" element={<MonParcours/>}/>
-                <Route path="/therapie-systemique" element={<Therapiesystemique/>}/>
-                <Route path="/psychocorporelle" element={<PsychoCorporelle/>}/>
-            </Routes>        
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/accueil" element={<Home />} />
+            <Route path="/mon-parcours" element={<MonParcours />} />
+            <Route path="/therapie-systemique" element={<Therapiesystemique />} />
+            <Route path="/psychocorporelle" element={<PsychoCorporelle />} />
+          </Routes>
         </BrowserRouter>
-
+      </ServicesProvider>
     </div>
   );
 }
