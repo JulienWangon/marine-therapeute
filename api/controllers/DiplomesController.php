5d0b3d87bc231b7d9be5315bd96fc0516acae991
Julien Wangon <julien.wangon@gmail.com>
2024-03-04 22:09:49 +0100

    ADD/ create Diplome controller file

---
+++ api/controllers/DiplomesController.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__ . '/../models/Diplomes.php';
+require_once __DIR__ . '/../repository/DiplomesRepository.php';
+require_once __DIR__ . '/../classes/ResponsesHelper.php';
+
+class DiplomesController {
+  
+  private $diplomesRepository;
+
+  public function __construct(DiplomesRepository $diplomesRepository) {
+    $this->diplomesRepository = $diplomesRepository;
+  }
+
+  public function getAllDiplomes() {
+    try {
+      $diplomes = $this->diplomesRepository->getDiplomes();
+
+      if (empty($diplomes)) {
+        ResponseHelper::sendResponse(['status' => 'error', 'message' => 'Aucunes informations trouvées.']);
+        return;
+      }
+
+      $diplomesArray = array_map(function($diplome) {
+        return [
+          'idDiplome' => $diplome->getId(),
+          'diplome' => $diplome->getDiplomes(),
+          'intitule' => $diplome->getIntitule(),
+          'lieu' => $diplome->getLieu()
+        ];
+      }, $diplomes);
+      ResponseHelper::sendResponse(['status' => 'success', 'data' => $diplomesArray]);
+    } catch (Exception $e) {
+      ResponseHelper::sendResponse(['status' => 'error', 'message' => $e->getMessage()], 400);
+    }
+  }
+}
