be31017737cce13d32a287d68a70b3cebb2d735b
Julien Wangon <julien.wangon@gmail.com>
2024-03-04 19:53:12 +0100

    ADD: crreate neovie section

--- marine-front/src/pages/public/Home/Home.js
+++ marine-front/src/pages/public/Home/Home.js
@@ -11,7 +11,10 @@ import logo3 from '../../../assets/img/logo3.webp';
 import logo4 from '../../../assets/img/logo4.webp';
 import marine from '../../../assets/img/marine.webp';
 import plante from '../../../assets/img/plante.webp';
-
+import neovie from '../../../assets/img/neovie.webp';
+import impot from '../../../assets/img/impot.webp';
+import cheque from '../../../assets/img/cheque.webp';
+import ethique from '../../../assets/img/ethique.webp';
 
 import SocialMedia from '../../../components/SocialMedia/SocialMedia';
 import SectionServices from '../../../components/ServicesCard/SectionServices';
@@ -120,7 +123,29 @@ const Home = () => {
               </ul>
           </div>
           <p className="infoText">Merci de prévenir le plus tôt possible d'un changement ou d'une annulation. Tout rendez-vous non honoré et non justifié moins de 48 heures en avance sera dû.</p>
-          
+          <p className="asterixInfo">* La prestation délivrée par ce professionnel ne fait pas l'objet d'un conventionnement. Dès lors, elle n'est pas prise en charge par la sécurité sociale. Certaines mutuelles peuvent rembourser ses services, vous pouvez lui demander une facture. Le montant des honoraires fixé est cependant déterminé avec tact et mesure.</p>
+          <div className="neovieWrapper">
+              <div className="neovieLogo">
+                  <h3 className="neovieTitle">Retrouvez-moi aussi sur</h3>
+                 <a href="https://www.neovieonline.com/marine-ottaviani-praticienne-en-constellations-familiales-f1345483.html"><img className="logoNeovie" src={neovie} alt="logo neovie"/></a>
+              </div>
+              <div className="impotWrapper">
+                <img className="impotCheque" src={impot} alt="logo crédit d'impot"/>
+                <p className="impotText">Réglement possible par chèque emploi service universel CESU et crédit d'impôts accessible</p>
+                <img className="impotCheque" src={cheque} alt="logo cheque emploi service"/>
+              </div>
+          </div>
+        </section>
+        <section className="sixSection">
+          <img className="ethiqueImg" src={ethique} alt="un livre et un cadenas"/>
+          <h2>Ethique professionnelle</h2>
+          <p className="infoText">En conformité avec le code de déontologie de la FF2P (Fédération Française de Psychanalyse et de Psychothérapie) à laquelle je suis adhérente, je m'engage à respecter le secret professionnel et la confidentialité de nos échanges.</p>
+          <p className="infoText">Je m'autorise à déroger à cette règle avec votre accord en cas de danger possible pour vous, dans le cadre d'éventuelles violences que vous subiriez ou si vos difficultés sont en dehors de mon champ de compétences.</p>
+          <p className="infoText">Les données transmises ne sont utilisées qu'à visée d'orientation et réduites au strict nécessaire.</p>
+          <p className="infoText">Les méthodes utilisées s'inscrivent dans la relation d'aide, hors cadre médical et réglementé. Les thérapies proposées ne se substituent en rien à un acte ou traitement médical dont elles sont peut-être complémentaires. Je soumets ma pratique à une supervision régulière de mon travail à des superviseurs expérimentés. Cette supervision m'est indispensable selon moi et m'aide à être dans une relation thérapeutique saine, pour mieux accompagner mes consultants selon leur problèmatique et les résonnances chez moi.</p>
+          <p className="infoText">Je m'engage également à une formation continue en participant fréquemment à des stages, des séminaires, des ateliers et des perfectionnements professionnels.</p>
+
+
         </section>
       </main>
 
