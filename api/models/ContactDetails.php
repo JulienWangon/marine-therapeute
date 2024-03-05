7a4afbf20357045109a9d6cf5e0f91f4a52e9ff8
Julien Wangon <julien.wangon@gmail.com>
2024-03-05 21:50:10 +0100

    UPDATE: change contact info parameter

--- api/models/ContactDetails.php
+++ api/models/ContactDetails.php
@@ -1,38 +1,68 @@
 <?php
 
 class ContactDetails {
-    private ?int $idContactDetail;
-    private string $address;
+    private ?int $id;
+    private string $numero;
+    private string $rue;
+    private int $codePostale;
+    private string $ville;
     private string $phone;
 
-    public function __construct(string $address = null, string $phone = null, ?int $idContactDetail = null ) {
-        $this->idContactDetail = $idContactDetail;
-        $this->address = $address;
+    public function __construct(int $numero, string $rue, int $codePostale, string $ville, string $phone, ?int $id = null ) {
+        $this->numero = $numero;
+        $this->rue = $rue;
+        $this->codePostale = $codePostale;
+        $this->ville = $ville;
         $this->phone = $phone;
+        $this->id = $id;   
     }
 
-    public function getIdContactDetail() :int {
-        return $this->idContactDetail;
+    public function getId(): ?int {
+        return $this->id;
     }
 
-    public function getAddress() :string {
-        return $this->address;
+    public function getNumero(): string {
+        return $this->numero;
     }
 
-    public function getPhone() :string {
+    public function getRue(): string {
+        return $this->rue;
+    }
+
+    public function getCodePostale(): int {
+        return $this->codePostale;
+    }
+
+    public function getVille(): string {
+        return $this->ville;
+    }
+
+    public function getPhone(): string {
         return $this->phone;
     }
 
-    public function setIdContactDetail($idContactDetail) :void {
-        $this->idContactDetail = $idContactDetail;
+    public function setId(?int $id): void {
+        $this->id = $id;
     }
 
-    public function setAddress($address) :void {
-        $this->address = $address;
+    public function setNumero(string $numero): void {
+        $this->numero = $numero;
     }
 
-    public function setPhone($phone) :void {
+    public function setRue(string $rue): void {
+        $this->rue = $rue;
+    }
+
+    public function setCodePostale(int $codePostale): void {
+        $this->codePostale = $codePostale;
+    }
+
+    public function setVille(string $ville): void {
+        $this->ville = $ville;
+    }
+
+    public function setPhone(string $phone): void {
         $this->phone = $phone;
-    }          
+    }  
 }
 
