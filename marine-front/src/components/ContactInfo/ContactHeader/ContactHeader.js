029b0335b576a183bd555c3d44daaddc7c79232e
Julien Wangon <julien.wangon@gmail.com>
2024-03-05 22:59:30 +0100

    ADD: create contact header component

---
+++ marine-front/src/components/ContactInfo/ContactHeader/ContactHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactHeader = ({ numero, rue, codePostal, ville, phone}) => {
+  return (
+    <div className="contactHeader">
+        <span className="adresse">{numero}, {rue} {codePostal} {ville}</span>
+        <a className="phone" href={`tel:0${phone}`}>0{phone}</a>
+      
+    </div>
+  );
+};
+
+export default ContactHeader;
