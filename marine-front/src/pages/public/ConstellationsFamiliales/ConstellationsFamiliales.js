044a821323ab5d5fbca8ce24041844963635c75e
Julien Wangon <julien.wangon@gmail.com>
2024-03-07 15:13:33 +0100

    ADD: create new page

---
+++ marine-front/src/pages/public/ConstellationsFamiliales/ConstellationsFamiliales.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../../components/common/Navbar/Navbar';
+
+const ConstellationsFamiliales = () => {
+  return (
+    <>
+      <Navbar/>
+      Ici page des constellations
+    </>
+  );
+};
+
+export default ConstellationsFamiliales;
