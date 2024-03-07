c1e2a701dc8536632a091c71642c7d66280147e0
Julien Wangon <julien.wangon@gmail.com>
2024-03-07 21:37:47 +0100

    UPDATE: add methode for email validation

--- api/classes/Validator.php
+++ api/classes/Validator.php
@@ -16,6 +16,24 @@ class Validator {
     return empty($this->errors["json"]);
   }
 
+  //Validation du format d'un email
+  public function validateEmail($email) {
+
+    // Check if e-mail exists
+    if(!$email || $email == "") {
+        $this->errors["email"][] = ["status" => "error", "message" => "L'adresse e-mail est requise."];
+        return false;
+    }
+
+    // Check e-mail format with filter_var()
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $this->errors["email"][] = ["status" => "error", "message" => "L'adresse e-mail n'est pas valide."];
+        return false;
+    }
+
+    return empty($this->errors["email"]);
+  }
+
    //Retourner les erreurs de validation
   public function getErrors() {
     return $this->errors;
