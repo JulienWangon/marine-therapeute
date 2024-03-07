72d53346e43332648f4fbde0b0b7cf00a720ce6e
Julien Wangon <julien.wangon@gmail.com>
2024-03-07 21:23:17 +0100

    UPDATE: add validate json format method

--- api/classes/Validator.php
+++ api/classes/Validator.php
@@ -4,4 +4,17 @@ class Validator {
   
   protected $errors = [];
 
-}
+
+  //Validation du format JSON
+  public function validateJsonFormat($jsonData) :bool {
+
+    if ($jsonData === null && json_last_error() !== JSON_ERROR_NONE) {
+          $this->errors["json"][] = ["status" => "error", "message" => "Format JSON invalide " . json_last_error_msg()];
+          return false;
+    }
+
+    return empty($this->errors["json"]);
+  }
+
+  }
+
