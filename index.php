c985b55937d32fcbe6045d018d72356c93b6c03f
Julien Wangon <julien.wangon@gmail.com>
2024-03-08 16:57:52 +0100

    UPDATE: add autoload file

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
 
+require __DIR__ . '/api/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/api');
+$dotenv->load();
 
 require_once '../marine-therapeute/api/routes/router.php';
